ac0db2b32f742c212b8610917dc823c44ffd9c73
Thomas Trudzinski <thomastrudzinski@gmail.com>
2025-03-24 22:39:11 -1000

    added scramble

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Scrambler from "./(main)/components/scrambler";
+
 export default function HomePage() {
   return (
     <main className="flex h-full flex-col items-center justify-center space-y-6 bg-slate-700 text-white">
       <div className="font-bebas leading-0 flex flex-col items-end text-[160px] leading-[0.80] tracking-wide">
         <div className="text-orange-500">REACHER</div>
-        <div className="text-6xl text-white">KILLS</div>
+
+        <Scrambler text="kills" className="h-[40px] text-4xl" />
+        {/* <div className="text-2xl text-white">KILLS</div> */}
       </div>
       <div className="font-sometype max-w-[600px] px-10 pt-10 text-center text-xl">
         A fan-made guide tracking every bad guy Jack Reacher takes down, episode
