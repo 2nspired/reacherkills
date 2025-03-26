2a83c2305d388b53c07d342fe919c57bb14ecbdb
Thomas Trudzinski <thomastrudzinski@gmail.com>
2025-03-26 11:18:15 -1000

    accent color to easily change theme

--- src/app/(main)/_components/Logo.tsx
+++ src/app/(main)/_components/Logo.tsx
@@ -4,13 +4,11 @@ import Link from "next/link";
 export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/">
-      <div
-        className={`flex flex-col px-6 font-bebas tracking-wide sm:flex-row ${className}`}
-      >
-        <div className="flex flex-col pt-[5px] text-orange-500">REACHER</div>
+      <div className={`flex px-6 font-bebas tracking-wide ${className}`}>
+        <div className="text-accent flex flex-col pt-[5px]">REACHER</div>
         <Scrambler
           text="kills"
-          className="pt-[5px] leading-[.80] sm:ml-3 sm:leading-none"
+          className="pt-[5px] leading-[.80] sm:leading-none"
         />
       </div>
     </Link>
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -4,31 +4,34 @@ import Header from "../(main)/_components/Header";
 export default function HomePage() {
   return (
     <div>
-      <div className="mx-auto w-full max-w-[1720px]">
+      <div className="mx-auto w-full max-w-7xl">
+        {/* <div className="mx-auto w-full max-w-[1720px]"> */}
         <Header />
       </div>
-      <div className="relative flex h-[900] w-screen flex-col items-center justify-center">
+      <div className="relative flex h-[900px] w-screen flex-col items-center justify-center md:h-[1075px]">
+        {/* TODO: NEED A NEW IMAGE WITH REACHER ON RIGHT, NOT CENTERED FOR LARGER VIEWS */}
         <Image
           src="/reacher-images/reacher-2560x1440-06.jpg"
-          className="h-auto max-w-[2560px] object-cover object-left"
+          className="h-auto max-w-[2560px] scale-x-[-1] transform object-cover object-center md:scale-x-100 md:object-left"
           alt="Reacher"
           fill={true}
           priority={true}
           unoptimized
         />
         <div className="absolute left-0 top-0 h-full w-full">
-          <div className="mx-auto max-w-[1720px]">
+          <div className="mx-auto max-w-7xl">
+            {/* <div className="mx-auto max-w-[1720px]"> */}
             <div className="py-6">
-              <div className="flex flex-col space-y-1 px-6 font-bebas text-[180px] font-bold leading-[.90] tracking-wide">
-                <div className="text-orange-500">REACHER</div>
+              <div className="flex flex-col space-y-1 px-6 font-bebas text-[8rem] font-bold leading-[.90] tracking-wide md:text-[180px]">
+                <div className="text-accent">REACHER</div>
                 <div className="leading-[.60]">KILLS</div>
               </div>
               <div className="flex flex-row items-start justify-between">
-                <div className="px-6 pt-3 font-bebas text-8xl leading-[.90] tracking-wide">
+                <div className="px-6 pt-3 font-bebas text-4xl leading-[.90] tracking-wide md:text-8xl">
                   <div>EVERY BAD GUY.</div>
                   <div>EVERY EPISODE.</div>
                 </div>
-                <div className="mt-2 w-96 px-6 text-right font-bebas text-6xl">
+                <div className="mt-2 hidden w-96 px-6 text-right font-bebas text-6xl">
                   <div>Total Kills</div>
                   <div>Methods &</div>
                   <div>Weapons Used</div>
@@ -36,8 +39,10 @@ export default function HomePage() {
               </div>
 
               <div className="mt-14 px-6">
-                <div className="font-bebas text-6xl tracking-wide">SITREP</div>
-                <div className="max-w-[800px] font-sometype text-xl">
+                <div className="font-bebas text-4xl tracking-wide md:text-6xl">
+                  SITREP
+                </div>
+                <div className="max-w-[800px] font-sometype md:text-xl">
                   A formidable ex–Military Police officer known only as Jack
                   Reacher has been spotted dispensing vigilante justice across
                   small-town America. Targets include corrupt businessmen, dirty
@@ -46,7 +51,7 @@ export default function HomePage() {
                   but.
                 </div>
               </div>
-              <div className="mt-10 flex flex-col space-y-2 px-6">
+              <div className="mt-10 flex flex-col items-center space-y-2 px-6 md:items-start">
                 <div className="font-sometype font-bold">watch on:</div>
                 <a
                   href="https://www.amazon.com/gp/video/detail/B09ML1GHXS/ref=atv_sr_fle_c_sr6eee57_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B09ML111MB&qid=1742970427979"
@@ -70,9 +75,24 @@ export default function HomePage() {
         </div>
       </div>
       {/* SUMMARY SECTION */}
-      <div>
-        <div>summary section</div>
-      </div>
+      {/* <div className="grid grid-rows-3 gap-6 p-6 text-black sm:grid-cols-3">
+        <div className="row-start-1 border border-black p-6 text-center md:col-start-1 md:row-start-auto">
+          <div className="font-bebas text-4xl">total kills</div>
+          <div className="font-sometype text-2xl">200</div>
+        </div>
+        <div className="row-start-2 border border-black p-6 text-center md:col-start-2 md:row-start-auto">
+          <div className="font-bebas text-4xl">weapons</div>
+          <div className="font-sometype text-2xl">bare hands</div>
+          <div className="font-sometype text-2xl">gun</div>
+          <div className="font-sometype text-2xl">knife</div>
+        </div>
+        <div className="row-start-3 border border-black p-6 text-center md:col-start-3 md:row-start-auto">
+          <div className="font-bebas text-4xl">methods</div>
+          <div className="font-sometype text-2xl">choked</div>
+          <div className="font-sometype text-2xl">stabbed</div>
+          <div className="font-sometype text-2xl">shot</div>
+        </div>
+      </div> */}
     </div>
   );
 }
