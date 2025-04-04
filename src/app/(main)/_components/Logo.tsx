982c084c881eea3ae59a40fc0e6848514a86fbcb
Thomas Trudzinski <thomastrudzinski@gmail.com>
2025-04-04 12:16:25 -1000

    usedForwardRef and useImperativeHandle to trigger scramble on container hover

--- src/app/(main)/_components/Logo.tsx
+++ src/app/(main)/_components/Logo.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import Scrambler from "~/app/(main)/_components/Scrambler";
 import Link from "next/link";
+import { useRef } from "react";
 
 export default function Logo({ className }: { className?: string }) {
+  const scramblerRef = useRef<{ triggerScramble: () => void }>(null);
+
+  const handleHover = () => {
+    scramblerRef.current?.triggerScramble();
+  };
+
   return (
     <Link href="/">
-      <div className={`flex px-6 font-bebas tracking-wide ${className}`}>
-        <div className="text-accent flex flex-col pt-[5px]">REACHER</div>
+      <div
+        onMouseEnter={handleHover}
+        onFocus={handleHover}
+        className={`font-bebas tracking-wide ${className}`}
+      >
+        <div className="flex flex-col pt-[5px] text-accent">REACHER</div>
         <Scrambler
+          ref={scramblerRef}
           text="kills"
           className="pt-[5px] leading-[.80] sm:leading-none"
         />
